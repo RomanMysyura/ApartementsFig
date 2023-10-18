a9f1bf97ae17cbf54ccf8344d59752fafa0f4f61
Adrian Poncelas Juncarol <aponcelas@cendrassos.net>
2023-10-18 15:41:55 +0200

    prova 1

--- src/views/menu.php
+++ src/views/menu.php
@@ -1,24 +1,40 @@
-<div class="container d-flex justify-content-between align-items-center py-3 bg-info ">
-    <div>
-        <a href="index.php">
-            <img src="imatges/ApartamentsFiguerencs.png" alt="Logo" width="50" height="50">
-        </a>
-
-    </div>
-    <div>
-        <a class="" href="index.php?r=reservar"><button type="button" class="btnlogin btn btn-primary"
-                id="BotonLogin">Reservar</button></a>
-        <a class="" href="index.php?r=contactar"><button type="button" class="btnlogin btn btn-primary"
-                id="BotonLogin">Contactar</button></a>
-        <a class="" href="index.php?r=ubicacio"><button type="button" class="btnlogin btn btn-primary"
-                id="BotonLogin">Ubicació</button></a>
-    </div>
-    <div>
-        <a class="" href="index.php?r=login"><button type="button" class="btnlogin btn btn-primary"
-                id="BotonLogin">Entrar</button></a>
-        <a class="" href="index.php?r=signup"><button type="button" class="btnlogin btn btn-primary"
-                id="BotonLogin">Registrar</button></a>
-    </div>
-    
-
+<div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Logo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php?r=reservar">Reservar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?r=contactar">Contactar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?r=ubicacio">Ubicació</a>
+                    </li>
+                </ul>
+                <form class="d-flex mx-auto text-center" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <ul class="navbar-nav me-right mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item" href="index.php?r=signup"><strong>Registrar-se</strong></a>
+                            </li>
+                            <li><a class="dropdown-item" href="index.php?r=login">Iniciar sessió</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </div>
