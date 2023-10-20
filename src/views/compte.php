96a1bff0c1a123b57bd44adbf7891b7c5b9a5f38
Adrian Poncelas Juncarol <aponcelas@cendrassos.net>
2023-10-20 16:35:43 +0200

    compte_1

--- src/views/compte.php
+++ src/views/compte.php
@@ -1,13 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>El meu compte</title>
     <?php controllerLibs() ?>
 </head>
-<body>
-<?php controllerMenu() ?>
-    <h3>COMPTE</h3>
+<body class="p-0 m-0 border-0 bd-example m-0 border-0">
+    <?php controllerMenu() ?>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 mb-3 mb-md-0">
+                <!-- Sección izquierda -->
+                <div class="bg-primary p-3 bg-dark rounded">
+                    <div class="p-3 text-center">
+                        <i class="fa-solid fa-user fa-3x d-block mx-auto" style="color: #ffffff;"></i>
+                    </div>
+                    <h1 class="mt-1 text-center text-white">DADES PERSONALS</h1>
+                    <div class="p-3">
+                        <ul class="list-group list-group-flush rounded">
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">Nom:</div>
+                                    Nom d'usuari
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">Cognoms:</div>
+                                    Cognoms del usuari
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">Telèfon:</div>
+                                    Telèfon del usuari
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">Correu Electrònic:</div>
+                                    Correu Electrònic del usuari
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mt-3 mb-3 mt-md-0">
+                <!-- Sección derecha -->
+                <div class="bg-second p-3 bg-dark rounded">
+                    <div class="p-3 text-center">
+                        <i class="fa-solid fa-house fa-3x d-block mx-auto" style="color: #ffffff;"></i>
+                    </div>
+                    <h1 class="mt-1 text-center text-white">RESERVES</h1>
+                    <div class="p-3">
+                        <div class="accordion" id="accordionExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        Accordion Item #1
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type of="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Accordion Item #2
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        Accordion Item #3
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
