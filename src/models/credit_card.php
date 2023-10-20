1f98be7e1c57e1887e90a5fb22fdbd82998e875f
Adrian Poncelas Juncarol <aponcelas@cendrassos.net>
2023-10-20 19:46:01 +0200

    compte_3

---
+++ src/models/credit_card.php
@@ -0,0 +1,40 @@
+<?php
+// Inicializa la variable de error
+$error_message = "";
+
+// Verificar si el formulario se ha enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Incluir el archivo de conexión a la base de datos
+    include '../src/models/ModelConnectBDD.php';
+
+    // Obtener los datos del formulario
+    $credit_card = $_POST["credit_card"] ?? "";
+
+    // Verificar si todos los campos obligatorios están presentes
+    if (empty($credit_card)) {
+        $error_message = "Por favor, complete todos los campos del formulario.";
+    } else {
+        // Obtener el nombre de usuario (puedes obtenerlo de la sesión o como mejor se adapte a tu sistema)
+        $user_name = "Adrià"; // Reemplaza con la forma en que obtienes el nombre de usuario
+
+        // Preparar la consulta SQL para actualizar la tarjeta de crédito del usuario
+        $sql = "UPDATE users SET card_number = :credit_card WHERE name = :user_name";
+
+        try {
+            // Preparar y ejecutar la consulta
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(":credit_card", $credit_card);
+            $stmt->bindParam(":user_name", $user_name);
+            $stmt->execute();
+
+            // Redirigir a la página de perfil u otra después de la actualización exitosa
+            header("Location: index.php?r=compte"); 
+            exit();
+        } catch (PDOException $e) {
+            // Manejar errores en caso de fallo en la actualización
+            $error_message = "Error al actualizar la tarjeta de crédito: " . $e->getMessage();
+        }
+    }
+}
+?>
+
---
+++ src/models/reserves.php
@@ -0,0 +1,37 @@
+<?php
+include '../src/models/ModelConnectBDD.php';
+
+$sql = "SELECT r.* FROM reservation r INNER JOIN users u ON r.id_user = u.id_user WHERE u.name = 'Adrià'";
+
+$result = $conn->query($sql);
+
+if ($result->rowCount() > 0) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+        $accordionTitle = "Reserva #" . $row['id_reserved'];
+
+        
+        echo '<div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading' . $row['id_reserved'] . '">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse' . $row['id_reserved'] . '" aria-expanded="true" aria-controls="collapse' . $row['id_reserved'] . '">
+                            ' . $accordionTitle . '
+                        </button>
+                    </h2>
+                    <div id="collapse' . $row['id_reserved'] . '" class="accordion-collapse collapse show" aria-labelledby="heading' . $row['id_reserved'] . '" data-bs-parent="#reservationsAccordion">
+                        <div class="accordion-body">
+                            <strong>ID Reserva:</strong> ' . $row['id_reserved'] . '<br>
+                            <strong>Fecha de Entrada:</strong> ' . $row['entry_date'] . '<br>
+                            <strong>Fecha de Salida:</strong> ' . $row['output_date'] . '<br>
+                            <strong>Estado:</strong> ' . $row['state'] . '<br>
+                            <strong>Precio:</strong> ' . $row['price'] . '<br>
+                        </div>
+                    </div>
+                </div>
+            </div>';
+
+    }
+} else {
+    echo "No se encontraron reservas para el usuario 'Adrià'.";
+}
+?>
