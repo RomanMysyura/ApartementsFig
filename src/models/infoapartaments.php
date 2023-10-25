98e257c556180f95470c64839a76f7cbd2b214de
Adrian Poncelas Juncarol <aponcelas@cendrassos.net>
2023-10-26 00:48:09 +0200

    infoapartaments-2

--- src/models/infoapartaments.php
+++ src/models/infoapartaments.php
@@ -4,103 +4,26 @@ include '../src/models/ModelConnectBDD.php';
 if (isset($_GET['id'])) {
     $apartmentId = $_GET['id'];
 
-    // Consulta SQL para obtener información del apartamento por ID
     $sql = "SELECT * FROM apartment WHERE id_apartment = :id";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':id', $apartmentId, PDO::PARAM_INT);
     $stmt->execute();
 
-    
-
     if ($stmt->rowCount() > 0) {
         $apartment = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        echo '<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">';
-
-        $active = 'active';
-
-        // Muestra las imágenes del apartamento utilizando los datos de la consulta SQL
-        echo '<div class="carousel-item ' . $active . '">
-            <img src="' . $apartment["image_path"] . '" class="d-block w-100" alt="Image for ' . $apartment["title"] . '">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>' . $apartment["title"] . '</h5>
-                <p>' . $apartment["short_description"] . '</p>
-            </div>
-        </div>';
-
-        echo '</div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>';
-
-        echo '<ol class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Nom:</div>';
-        echo $apartment['title']; 
-        echo '</div>
-            </li>
-
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Adreça postal:</div>';
-        echo $apartment['postal_address'];
-        echo '</div>
-            </li>
-
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Longitud:</div>';
-        echo $apartment['length'];
-        echo '</div>
-            </li>
-
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Latitud:</div>';
-        echo $apartment['latitude'];
-        echo '</div>
-            </li>
-
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Descripció:</div>';
-        echo $apartment['short_description'];
-        echo '</div>
-            </li>
-
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Metres quadrats:</div>';
-        echo $apartment['square_metres'];
-        echo '</div>
-            </li>
-
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Número d\'habitacions:</div>';
-        echo $apartment['number_rooms'];
-        echo '</div>
-            </li>
-
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">Serveis i extres:</div>';
-        echo $apartment['services_extres'];
-        echo '</div>
-            </li>
-        </ol>';
-
-    } else {
-        echo "No se encontró información para el apartamento con ID $apartmentId";
+        $apartmentData = array(
+            'Nom' => $apartment['title'],
+            'Adreça postal' => $apartment['postal_address'],
+            'Longitud' => $apartment['length'],
+            'Latitud' => $apartment['latitude'],
+            'Descripció' => $apartment['short_description'],
+            'Metres quadrats' => $apartment['square_metres'],
+            'Número d\'habitacions' => $apartment['number_rooms'],
+            'Serveis i extres' => $apartment['services_extres']
+        );
     }
+
 } else {
     echo "Falta el parámetro 'id' en la URL.";
 }
--- src/views/infoapartaments.php
+++ src/views/infoapartaments.php
@@ -1,48 +1,65 @@
-
+<?php include '../src/models/infoapartaments.php'?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title><?php echo $apartment['title'];?></title>
-
-
+    <title><?= $apartment['title']; ?></title>
 </head>
 <body>
-<?php include '../src/views/menu.php';?>
-
-    <div class="container mt-5">
+    <?php include '../src/views/menu.php'; ?>
+    <div class="container">
         <div class="row">
-            <div class="col-12">
-                <div class="bg-primary p-3 bg-dark rounded">
-
-                    <h1 class="text-white">DADES DEL APARTAMENT</h1>
-
-                    <?php include '../src/models/infoapartaments.php';?>
-
-                    <h1 class="text-white">GOOGLE MAPS</h1>
-                    
-
-                    <div id="map" data-latitude="<?php echo $apartment['latitude']; ?>" data-longitude="<?php echo $apartment['length']; ?>"></div>
-
-
-                    
+            <div class="col-md-7 mx-auto">
+                <div class="p-3">
+                    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner rounded">
+                            <?php $active = 'active'; ?>
+                            <div class="carousel-item <?= $active ?>">
+                                <img src="<?= $apartment["image_path"] ?>" class="d-block w-100" alt="Image for <?= $apartment["title"] ?>">
+                            </div>
+                            <?php $active = ''; ?>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <ol class="list-group custom-list">
+                        <?php foreach ($apartmentData as $label => $value): ?>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold"><?= $label ?>:</div>
+                                    <?= $value ?>
+                                </div>
+                            </li>
+                        <?php endforeach; ?>
+                    </ol>
+                    <div id="map" data-latitude="<?= $apartment['latitude']; ?>" data-longitude="<?= $apartment['length']; ?>"></div>
                 </div>
             </div>
         </div>
     </div>
-    
-    <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"
-     integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg=="
-     crossorigin=""></script>
-     <script>
-            var apartmentId = <?php echo json_encode($apartmentId); ?>;
-            var latitude = <?php echo json_encode($apartment['latitude']); ?>;
-            var length = <?php echo json_encode($apartment['length']); ?>;
-    </script>
-    
-    
 
-     <script src="js/scripts.js"></script>
-     
+    <div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div>
+
+    <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"
+            integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg=="
+            crossorigin=""></script>
+    <script>
+        var apartmentId = <?= json_encode($apartmentId); ?>;
+        var latitude = <?= json_encode($apartment['latitude']); ?>;
+        var length = <?= json_encode($apartment['length']); ?>;
+    </script>
+
+    <script src="js/scripts.js"></script>
 </body>
 </html>
+
